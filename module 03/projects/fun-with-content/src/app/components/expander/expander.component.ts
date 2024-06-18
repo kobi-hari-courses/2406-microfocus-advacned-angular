271e7078c08a1235cedd67c44227212d089b0a48
Kobi Hari <kobi2294@yahoo.com>
2024-06-18 11:24:13 +0300

    Customizing the toggle button is now optional

--- module 03/projects/fun-with-content/src/app/components/expander/expander.component.ts
+++ module 03/projects/fun-with-content/src/app/components/expander/expander.component.ts
@@ -1,15 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, Input, ViewChild, contentChild, effect } from '@angular/core';
+import { ExpanderToggleDirective } from './expander-toggle.directive';
 
 @Component({
   selector: 'app-expander',
   templateUrl: './expander.component.html',
-  styleUrl: './expander.component.css'
+  styleUrl: './expander.component.css',
 })
 export class ExpanderComponent {
+  // @ContentChild(ExpanderToggleDirective)
+  // expanderToggle: ExpanderToggleDirective | undefined = undefined;
+
+  readonly expanderToggle = contentChild(ExpanderToggleDirective);
+
   isExpanded: boolean = true;
 
   toggle() {
     this.isExpanded = !this.isExpanded;
   }
 
+  ngOnInit() {
+
+  }
+
+  constructor() {
+    effect(() => {
+      console.log(' The expander toggle is: ', this.expanderToggle());      
+    })
+  }
+  ngAfterContentInit(): void {
+  }
 }
